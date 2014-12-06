957e0bb9f97d0e17901978c01b218224ce5285df
Parsha Pourkhomami <parshap+git@gmail.com>
2014-12-06 03:27:54 -0800

    Rename bootstrap to index.js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ var xtend = require("xtend");
 
 function scripts(opts) {
   var env = opts.env || process.env;
-  var b = browserify(__dirname + "/ui/bootstrap", opts);
+  var b = browserify(__dirname + "/ui", opts);
   b.transform(envify(env));
   if (env.NODE_ENV === "production") {
     b.transform({ global: true }, uglifyify);
