417bf075e21852341ee8efc440c10ccb6d07f8e6
Parsha Pourkhomami <parshap+git@gmail.com>
2014-12-06 12:13:31 -0800

    Schedule ui wip

--- ui/schedule.js
+++ ui/schedule.js
@@ -4,8 +4,6 @@
 var xtend = require("xtend");
 var React = require("react");
 var el = React.createElement;
-var caltrain = require("caltrain-schedule");
-var stations = require("../stations.json");
 var getStopName = require("./get-stop-name");
 
 function getFirstStop(scheduledTrip) {
@@ -43,7 +41,7 @@ function isSameDay(date1, date2) {
 }
 
 function getAmPm(date) {
-  if (date.getHours() > 12) {
+  if (date.getHours() >= 12) {
     return "PM";
   }
   else {
@@ -79,7 +77,7 @@ function formatDuration(ms) {
   return Math.round(minutes) + " min";
 }
 
-// ## Item - Scheduled stop
+// ## Scheduled Trip
 
 function renderTime(props) {
   return el("time", xtend({
@@ -94,7 +92,18 @@ function renderTrainName(props) {
   }, props));
 }
 
-var Item = React.createClass({
+function formatTimeTitle(departDate, arriveDate) {
+  return [
+    "Depart: ",
+    formatTime(departDate),
+    "\nArrive: ",
+    formatTime(arriveDate),
+    "\nDuration: ",
+    formatDuration(arriveDate - departDate),
+  ].join("");
+}
+
+var ScheduledTrip = React.createClass({
   getTrainName: function() {
     var scheduledTrip = this.props.scheduledTrip;
     var firstTripStop = getFirstStop(scheduledTrip);
@@ -103,26 +112,21 @@ var Item = React.createClass({
       headsign + scheduledTrip.trip.trip_short_name;
   },
 
-  getTripDuration: function() {
-    var scheduledTrip = this.props.scheduledTrip;
-    var firstTripStop = getFirstStop(scheduledTrip);
-    var lastTripStop = getLastStop(scheduledTrip);
-    return formatDuration(lastTripStop.date - firstTripStop.date);
-  },
-
   getTripStopsCount: function() {
     return this.props.scheduledTrip.tripStops.length;
   },
 
   render: function() {
     var scheduledTrip = this.props.scheduledTrip;
-    var firstStopDate = getFirstStop(scheduledTrip).date;
-    return el("div", {
+    var firstStop = getFirstStop(scheduledTrip);
+    var lastStop = getLastStop(scheduledTrip);
+    return el("article", {
       style: this.props.style,
       children: [
         renderTime({
-          dateTime: firstStopDate.toISOString(),
-          children: formatTime(firstStopDate),
+          dateTime: firstStop.date.toISOString(),
+          children: formatTime(firstStop.date),
+          title: formatTimeTitle(firstStop.date, lastStop.date),
         }),
         " ",
         renderTrainName({
@@ -141,8 +145,12 @@ module.exports = React.createClass({
     return getStopName(getFirstStop(this.props.schedule[0]).station);
   },
 
+  getArrivingStationName: function() {
+    return getStopName(getLastStop(this.props.schedule[0]).station);
+  },
+
   render: function() {
-    return el("div", {
+    return el("article", {
       children: [
         this.renderHeading(),
         this.renderSchedule(),
@@ -155,13 +163,15 @@ module.exports = React.createClass({
       children: [
         "Departures from ",
         el("em", null, this.getDepartingStationName()),
+        " to ",
+        el("em", null, this.getArrivingStationName()),
       ],
     });
   },
 
   renderSchedule: function() {
     return this.props.schedule.map(function(scheduledTrip) {
-      return Item({
+      return ScheduledTrip({
         scheduledTrip: scheduledTrip,
         style: {
           "margin-bottom": ".5rem",
