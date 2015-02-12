f2cae6d6e5bf80586a50c983394336b12270bd1a
Parsha Pourkhomami <parshap+git@gmail.com>
2015-02-12 02:13:27 -0800

    Split out board time component

--- ui/trip-header.js
+++ ui/trip-header.js
@@ -3,12 +3,11 @@
 
 var React = require("react");
 var el = React.createElement;
-var formatHours = require("../lib/time-utils").formatHours;
 var formatMinutes = require("../lib/time-utils").formatMinutes;
-var getAmPm = require("../lib/time-utils").getAmPm;
 var colors = require("./colors");
 var getFirstStop = require("./schedule-util").getFirstStop;
 var getLastStop = require("./schedule-util").getLastStop;
+var renderBoardTime = require("./board-time");
 
 function renderTrainName(props) {
   var firstTripStop = getFirstStop(props.scheduledTrip);
@@ -60,41 +59,7 @@ function getTrainTypeColor(type) {
   }
 }
 
-function renderBoardTime(props) {
-  var date = props.date;
-  return el("time", {
-    style: props.style,
-    dateTime: date.toISOString(),
-    children: [
-      renderBoardHours(date.getHours()),
-      ":",
-      formatMinutes(date.getMinutes()),
-      el("span", {
-        className: "dim",
-        style: {
-          "margin-left": ".4em",
-          "font-size": "65%",
-          "text-transform": "uppercase",
-        },
-        children: getAmPm(date),
-      }),
-    ],
-  });
-}
 
-function renderBoardHours(hours) {
-  hours = formatHours(hours);
-  if (hours.length < 2) {
-    return el("span", {
-      dangerouslySetInnerHTML: {
-        __html: "0" + hours,
-      },
-    });
-  }
-  else {
-    return hours;
-  }
-}
 
 
 function renderBoardDuration(props) {
